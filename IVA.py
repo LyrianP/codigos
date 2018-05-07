87855e6faa8a4dab140b17080e9aec553b7cc237
LyrianP <38957701+LyrianP@users.noreply.github.com>
2018-05-07 21:50:57 +0200

    Create IVA.py

---
+++ IVA.py
@@ -0,0 +1,10 @@
+# Calcular precio del IVA y precio final de un producto
+
+precio = 29.95
+IVA = 0.21
+precioIVA = precio * IVA
+preciototal = precio + precioIVA
+
+  print("El precio del IVA es", precioIVA, "€")
+  
+  print("El precio final es", preciototal, "€")
